025ce39ce31f77109b5f7e40af40bb93fdf98967
kazbek-d <kazbek_d@hotmail.com>
2025-10-30 21:13:23 +0100

    Added StorageFactory

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    // type  visibility  name
+    // type       visibility  name
     SimpleStorage public simpleStorage;
 
     function createSimpleStorageContract() public {
