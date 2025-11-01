67cfdeb2b219023d68d4c4c0545323aa7965e193
kazbek-d <kazbek_d@hotmail.com>
2025-11-01 09:29:41 +0100

    Added Interaction with Contracts ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,10 +4,25 @@ pragma solidity ^0.8.24;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    // type       visibility  name
-    SimpleStorage public simpleStorage;
+    // type         visibility  name
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256  _newSimpleStorageNumber) public {
+        // Address
+        // API - Application Binary Interface
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        simpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        // Address
+        // API - Application Binary Interface
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
